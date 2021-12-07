c268cc390cdab36748fc28ac20f64682a6c1c635
Sadowski <krystek23s@gmail.com>
2021-12-07 14:40:21 +0100

    [7.12.21]

--- 30.11/notatnik.py
+++ 30.11/notatnik.py
@@ -17,9 +17,30 @@ def save():
 def lock():
     if(input2["state"] == 'normal'):
         input2["state"] = 'disable'
+        input2['background'] = "gray"
     else:
         input2["state"] = 'normal'
+        input2['background'] = "white"
 
+patterns = ("<VirtualHost", "DocumentRoot", "ServerAdmin", "ServerName", "ErrorLog", "CustomLog", "<Directory")
+cmds = {"vHost":"", "docRot":"", "srvAdm":"", "srvNam":"", "errLog":"", "cstLog":"", "dir":""}
+cmdKeys = list(cmds.keys())
+def read():
+    s1 = input.get()
+    input2.delete(1.0, "end")
+    with open(s1, 'r') as reader:
+        row=0
+        content = reader.readlines()
+        for line in content:
+            lineCont = content[row]
+            ic = 0
+            for pattern in patterns:
+                if("#" not in lineCont and pattern in lineCont):
+                    lc = patterns.index(pattern)
+                    print(str(ic)+" "+lineCont)
+                    print(cmdKeys[ic])
+            input2.insert(str(row+1)+'.0',lineCont)
+            row+=1
     
 
 
@@ -38,6 +59,7 @@ scb.pack(side=RIGHT, fill=Y)
 scb.config(command=input2.yview)
 input2.pack()
 btn = Button(win, command=save, text="zapisz").pack()
+btn = Button(win, command=read, text="Otwórz").pack()
 btnLock = Button(win, command=lock, text="Zablokuj/Odblokij").pack()
 frame.pack()
 mainloop()
