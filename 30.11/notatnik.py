43ca85fe30dded3cabe16187a3e17d436b94cbce
Sadowski <krystek23s@gmail.com>
2021-12-14 14:21:53 +0100

    [14.12.21]

--- 30.11/notatnik.py
+++ 30.11/notatnik.py
@@ -1,5 +1,7 @@
 from os import name, write
+import re
 from tkinter import * 
+from re import *
 
 win = Tk()
 win.geometry("320x480")
@@ -8,8 +10,8 @@ def save():
     s1 = input.get()
     s2 = input2.get(1.0, END)
     input2["state"]='disable'
-    print(s1)
-    print(s2)
+    #print(s1)
+    #print(s2)
     with open(s1, "w") as writer:
         writer.write(s2)
     input2["state"]='normal'
@@ -36,12 +38,17 @@ def read():
             ic = 0
             for pattern in patterns:
                 if("#" not in lineCont and pattern in lineCont):
-                    lc = patterns.index(pattern)
-                    print(str(ic)+" "+lineCont)
-                    print(cmdKeys[ic])
+                    ic = patterns.index(pattern)
+                    parseLine(ic, lineCont)
+                    #print(cmdKeys[ic])
             input2.insert(str(row+1)+'.0',lineCont)
             row+=1
     
+def parseLine(ic, line):
+    pt = "^( |\t)*("+patterns[ic]+"){1}([\s\d:*\"][^>]+)(>)*"
+    m =re.match(pt, line)
+    if m is not None:
+        print(m.group(3))
 
 
 
@@ -49,7 +56,7 @@ frame  = Frame(win)
 scb = Scrollbar(frame, orient="vertical")
 fileName = Label(win, text="text").pack()
 input = Entry(win)
-input.insert(0, "Nowy-Plik")
+input.insert(0, "000-default.conf")
 input.pack()
 content = Label(frame, text="treść").pack()
 input2 = Text(frame, height=12, yscrollcommand=scb.set)
