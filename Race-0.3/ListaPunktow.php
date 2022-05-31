87d9205917f90f0b7603c45baf26463595584684
Sadowski <krystek23s@gmail.com>
2022-05-31 10:05:51 +0200

    [add] more random things

--- Race-0.3/ListaPunktow.php
+++ Race-0.3/ListaPunktow.php
@@ -1,16 +1,23 @@
 <?php
 class ListaPunktow {
-	
+	/** @var array $lista przechowuję liste punktów */
 	private $lista = array();
 	
 	public function __construct(){
 		print("Treśćxx<br/>");
 	}
-	
+	 /**
+	  * dodaje punkt do listy
+     * @param Int $pk point to add to list
+     */
 	public function dodaj( $pk )
 	{	// dodaj do listy[] punkt kontrolny
 		$this->lista[] = $pk;
 	}
+	 /**
+	  * wyświetla kod html z lista punktów
+     * @return String
+     */
 	public function wyswietl()
 	{	// wygeneruj kod HTML z listą PK
 		$out = '';		#var_dump($this->lista);
--- Race-0.3/PunktKontrolny.php
+++ Race-0.3/PunktKontrolny.php
@@ -1,34 +1,60 @@
 <?php
 class PunktKontrolny{
-	
+	/** @var int $id przechowuje id */
 	private $id;
+	/** @var array $zespol przechowuje zespół */
 	private $zespol;
+	/** @var int $punktyDostepne przechowuje dostępne kunkty */
 	private $punktyDostepne = 100;	// pkt
+	/** @var int $punktyZdobyte przechowuje zdobyte kunkty */
 	private $punktyZdobyte = 0;
+	/** @var int $czasDostepny przechowuje dostępny czas */
 	private $czasDostepny = 10*60;	// min/sek = (10*60)
+	/** @var int $czasRealizacji przechowuje czas realizacji */
 	private $czasRealizacji = 0;	// min/sek
+	/** @var String $nazwa przechowuje nazwe */
 	private $nazwa;
+	/** @var String $opis przechowuje opis */
 	private $opis;
 	
 	public function __construct( int $id, string $nazwa ){
 		$this->id = $id;
 		$this->nazwa = $nazwa;
 	}
+	/**
+	  * ustala dostępne kunkty
+     * @param Int $wartosc ilośc dostępnych punktów
+     */
 	public function ustalPunkty( int $wartosc ){
 		$this->punktyDostepne = $wartosc;
 	}
+	/**
+	  * ustawia dany zespół w gotowości
+     * @param Int $idZesp id zespołu do zameldowania
+     */
 	public function zamelduj( int $idZesp ){
 		$ts = date("Y-m-d H:i:s");	// 2022-05-31 08:28:08
 		$this->zespol = array('id'=>$idZesp,'czasWe'=>$ts );
 	}
+	/**
+	  * usuwa zespól z gry
+     * @param Int $idZesp id zespołu do odmeldowania
+     */
 	public function odmelduj( int $idZesp ){
 		$ts = date("Y-m-d H:i:s");	// 2022-05-31 08:28:08
 		$this->zespol['czasWy'] = $ts+515;
 	}
+	/**
+	  * zlicza pinkty
+     * 
+     */
 	public function zalicz(){
 		$this->odmelduj($this->zespol['id']);
 		$this->obliczPunkty();
 	}
+	/**
+	  * oblicza punkty na podstawie pozostałego czasu
+     */
 	private function obliczPunkty(){
 		# $czasDostepny = 3*60; // min
 		# $punktyDostepne = 360; //pkt
@@ -41,6 +67,10 @@ class PunktKontrolny{
 		#var_dump($this->czasDostepny);		var_dump($this->czasRealizacji);		var_dump($pkt);
 		$this->punktyZdobyte = $pkt;
 	}
+	/**
+	  * wyświetla html z opisem
+	  * @return String Html z opisem
+     */
 	public function wyswietlOpis(){
 		$rn = "<br/>\r\n";
 		$out = 'Punkt kontrolny #'.$this->id.$rn;
